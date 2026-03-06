35125a9de929c97599cfe6bdfefaa7e6087abb18
Microwave <135585343+dohuyduc2002@users.noreply.github.com>
2026-03-06 10:19:30 +0700

    Update solution for text-chunking

--- text-chunking/text-chunking.py
+++ text-chunking/text-chunking.py
@@ -7,8 +7,7 @@ def text_chunking(tokens, chunk_size, overlap):
     out = []
     if chunk_size > len(tokens) and len(tokens) != 0:
         return [tokens]
-    # if len(tokens) == 0:
-    #     return tokens
+
     for i in range(0, len(tokens), step):
         if i + chunk_size > len(tokens):
             break
