64e2f5d71e861fee35e2dd8644fd7066f758cf4c
Microwave <135585343+dohuyduc2002@users.noreply.github.com>
2026-03-08 21:44:46 +0700

    Add solution for selu-activation

---
+++ selu-activation/selu-activation.py
@@ -0,0 +1,18 @@
+import numpy as np
+
+def selu(x, lam=1.0507009873554804934193349852946, alpha=1.6732632423543772848170429916717):
+    """
+    Apply SELU activation element-wise.
+    Returns a list of floats rounded to 4 decimal places.
+    """
+    # Write code here
+
+    result = []
+    for i in x:
+        if i > 0:
+            out = lam * i
+        else:
+            out = lam * alpha * (math.exp(i) - 1)
+            
+        result.append(out)
+    return result
